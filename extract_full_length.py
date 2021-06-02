58d0611bbf6c77893d9ee2a34d268c77e2f7cbe5
Jerry Chu <jerry955071@gmail.com>
2021-06-02 23:08:20 +0800

    add arg --min_length

--- extract_full_length.py
+++ extract_full_length.py
@@ -16,6 +16,8 @@ parser.add_argument('--ap_length', nargs=2, metavar='', default=[130, 60], type=
         help='AP length, separated by space (Default: 130 60)')
 parser.add_argument('--primer_cnfg', metavar='', required=False, 
                     help='Primer configuration. (Not required)')
+parser.add_argument('--min_length', metavar='', default=150, type=int,
+        help='Minimal output read length. (Default: 1)')
 parser.add_argument('--fusion_read', metavar='', required=False,
                     help='Write splitted fusion reads to this file.')
 parser.add_argument('input_fastq', metavar='input_fastq', help='Input fastq.')
@@ -38,6 +40,7 @@ def classifier(
     q_cutoff,
     ap_length,
     error_cutoff,
+    min_length,
     primer_cnfg,
     out_fusion
 ):
@@ -45,7 +48,7 @@ def classifier(
     # report start time and variables
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    print_to_stdout(f'Work start...\nCurrent time: {current_time}')
+    print_to_stdout(f'Work start...\nCurrent time: {current_time}\n')
     print_to_stdout('Parameters:')
     print_to_stdout(f'--input file: {input_dir}')
     print_to_stdout(f'--output file: {output_dir}')
@@ -71,8 +74,9 @@ def classifier(
     output = open(output_dir, 'w')
     output = open(output_dir, 'a')
 
-    fusion = open(out_fusion, 'w')
-    fusion = open(out_fusion, 'a')
+    if out_fusion:
+        fusion = open(out_fusion, 'w')
+        fusion = open(out_fusion, 'a')
 
     with open(input_dir, 'r') as f:
         read = seqtools.SeqFastq.read(f)
@@ -133,8 +137,9 @@ def classifier(
 
                             # output, make sure trimming were done
                             if new_read.cut_ploy:
-                                stats['FusionReads'] += 1
-                                new_read.write(fusion)
+                                if len(new_read.seq) >= min_length:
+                                    stats['FusionReads'] += 1
+                                    new_read.write(fusion)
 
             # body_hit == False
             else:
@@ -151,14 +156,16 @@ def classifier(
 
                     # output, make sure trimming were done
                     if read.cut_ploy:
-                        stats['FullLengthReads'] += 1
-                        read.write(output)
+                        if len(read.seq) >= min_length:
+                            stats['FullLengthReads'] += 1
+                            read.write(output)
 
             # get the next read
             read = seqtools.SeqFastq.read(f)
 
     output.close()
-    fusion.close()
+    if out_fusion:
+        fusion.close()
     
     print_to_stdout(f"InputReads: {stats['InputReads']}")
     print_to_stdout(f"LengthFailed: {stats['LengthFailed']}")
@@ -167,6 +174,7 @@ def classifier(
     print_to_stdout(f"FullLengthReads: {stats['FullLengthReads']}")
     print_to_stdout(f"BodyHits: {stats['BodyHits']}")
     print_to_stdout(f"FusionReads: {stats['FusionReads']}")
+    print_to_stdout("")
 
 
     now = datetime.now()
@@ -212,6 +220,7 @@ classifier(
     q_cutoff=args.q,
     ap_length=args.ap_length,
     error_cutoff=args.e,
+    min_length=args.min_length,
     primer_cnfg=primer_cnfg,
     out_fusion=out_fusion
 )
